98ce6522875255655b29c5e745d4dfe87cb03416
DESKTOP-4T2RKR7\admin <phungthinh18052003@gmail.com>
2023-03-31 23:30:49 +0700

    Hiển thị thời gian hệ thống

---
+++ Hiển thị thời gian của hệ thống/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Program
+{
+    class main
+    {
+        static void Main(string[] s)
+        {
+            DateTime localDate = DateTime.Now;
+            Console.WriteLine("DateTime Now is :" + localDate);
+        }
+    }
+}
