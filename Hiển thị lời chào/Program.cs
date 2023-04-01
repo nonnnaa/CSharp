6ea891348b78e6f680036249cbe7d2ce1ec0bffe
DESKTOP-4T2RKR7\admin <phungthinh18052003@gmail.com>
2023-04-01 12:39:47 +0700

    Hiển thị lời chào

---
+++ Hiển thị lời chào/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Program
+{
+    class solve
+    {
+        static void Main(string[] s)
+        {
+            Console.WriteLine("Enter your name: ");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Hello: " + Name);
+        }
+    }
+}
