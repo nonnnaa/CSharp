2d5ea5f7ad1f4a7683c6bf4ee8af94b72889baeb
DESKTOP-4T2RKR7\admin <phungthinh18052003@gmail.com>
2023-04-01 12:29:36 +0700

    Toán tử

---
+++ Toán tử/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Program
+{
+    class Solve
+    {
+        static void Main(string[] s)
+        {
+            Console.Write("Enter width: ");
+            float width = float.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            float height = float.Parse(Console.ReadLine());
+            Console.WriteLine("Area is: " + width  * height);
+        }
+    }
+}
