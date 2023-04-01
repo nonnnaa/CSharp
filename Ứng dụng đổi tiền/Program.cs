18c888d164e857ba7dd945c1456aeeb77be0ecbb
DESKTOP-4T2RKR7\admin <phungthinh18052003@gmail.com>
2023-04-01 12:49:31 +0700

    Ứng dụng đổi tiền

---
+++ Ứng dụng đổi tiền/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace solve
+
+{
+    class Program
+    {
+        static void Main(string[] s)
+        {
+            int x = 23000;
+            float USD = float.Parse(Console.ReadLine());
+            Console.WriteLine(USD * x);
+        }
+    }
+}
