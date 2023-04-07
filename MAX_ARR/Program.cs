7cdb497632594fe003d038d68cfce29625c7c1ab
DESKTOP-4T2RKR7\admin <phungthinh18052003@gmail.com>
2023-04-07 21:41:53 +0700

    update

---
+++ MAX_ARR/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+namespace Program
+{
+    class Solve
+    {
+        static void Main(string[] s)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for(int i=0 ; i<n ; i++)
+            {
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+            //Console.WriteLine(arr.Max());
+            int ans = int.MinValue;
+            for(int i=0 ; i<n ; i++)
+            {
+                ans = Math.Max(ans, arr[i]);
+            }
+            Console.WriteLine("Max of array is : " + ans);
+        }
+    }
+}
