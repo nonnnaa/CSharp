88d0d5283245fc48db18c653a19f3fa1161eaf11
DESKTOP-4T2RKR7\admin <phungthinh18052003@gmail.com>
2023-04-08 17:45:25 +0700

    update

---
+++ Tổng chéo chính/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+namespace Program
+{
+    class Solve
+    {
+        static void Main(string[] s)
+        {
+            // nhap do lon ma tran cap n
+            Console.Write("Enter n : ");
+            int n = Int32.Parse(Console.ReadLine());
+            // nhap mang
+            Console.WriteLine("Enter array : ");
+            int[,] arr = new int[n,n];
+            for(int i=0 ; i<n ; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                for(int j=0 ; j<n ; j++)
+                {
+                    arr[i,j] = Convert.ToInt32(input[j]);
+                }
+            }
+            int ans = 0;
+            for(int i=0 ; i<n ; i++)
+            {
+               ans += arr[i,i];
+            }
+            Console.WriteLine("The sum of the elements on the main diagonal of the matrix is : " + ans);
+        }
+    }
+}
