266ffe501857697ef2706c71cc11e31096f7f76f
DESKTOP-4T2RKR7\admin <phungthinh18052003@gmail.com>
2023-04-10 22:19:39 +0700

    update

---
+++ Đếm số lần xuất hiện của các kí tự trong chuỗi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+namespace Program
+{
+    class Solve
+    {
+        static void CountChar(string s)
+        {
+            int[] a = new int[127];
+            for(int i=0 ; i<s.Length ; i++)
+            {
+                a[Convert.ToInt32(s[i])]++;
+            }
+            for(int i=0 ; i<127 ;i++)
+            {
+                if(a[i] > 0)
+                {
+                    Console.WriteLine(Convert.ToChar(i) + " : " + a[i]);
+                }
+            }
+        }
+        static void Main()
+        {
+            string s = (Console.ReadLine());
+            CountChar(s);
+        }
+    }
+}
