49b8ae64322587af313e1f8640afb2dee9e16da2
DESKTOP-4T2RKR7\admin <phungthinh18052003@gmail.com>
2023-04-25 22:24:02 +0700

    update

---
+++ FAN/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace _Fan
+{
+    class Fan
+    {
+        public const int slow = 1;
+        public const int medium = 2;
+        public const int fast = 3;
+        private int speed {get; set;} = 1;
+        private bool on {get; set;} = false;
+        private double radius {get; set;} = 5;
+        private string color {get; set;} = "blue";
+        public Fan(){ }
+        public Fan(bool on, double radius,int speed, string color)
+        {
+            this.color = color;
+            this.radius = radius;
+            this.on = on;
+            this.speed = speed;
+        }
+        
+        public void Infor()
+        {
+            Console.WriteLine("========================");
+            if(on == true)
+            {
+                Console.WriteLine("Fan is on");
+                Console.WriteLine("Speed : " + speed);
+                Console.WriteLine("Color : " + color);
+                Console.WriteLine("Radius : " + radius);
+            }
+            else
+            {
+                Console.WriteLine("Fan is off");
+                Console.WriteLine("Color : " + color);
+                Console.WriteLine("Radius : " + radius);
+            }
+            Console.WriteLine("========================");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] s)
+        {
+            Fan fan1 = new Fan(true, 10, Fan.fast, "yellow");
+            Fan fan2 = new Fan(false, 5, Fan.medium, "blue");
+            fan1.Infor();
+            fan2.Infor();
+        }
+    }
+}
---
+++ StopWatch/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace _StopWatch
+{
+    class StopWatch
+    {
+        private DateTime startTime{get; set;}
+        private DateTime endTime{get; set;}
+        public StopWatch()
+        {
+            startTime = DateTime.Now;
+        }
+        public void Start()
+        {
+            startTime = DateTime.Now;
+        }
+        public void End()
+        {
+            endTime = DateTime.Now;
+        }
+        public double GetElapsedTime()
+        {
+            return (double)(endTime - startTime).TotalMilliseconds;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] s)
+        {
+            int[] arr = new int[100000];
+            Random random = new Random();
+            for(int i=0 ;i<100000 ; i++)
+            {
+                arr[i] = random.Next(0, 100000);
+            }
+            StopWatch watch = new StopWatch();
+            watch.Start();
+            SelectionSort(arr);
+            watch.End();
+            Console.Write(watch.GetElapsedTime());
+        }
+        static void SelectionSort(int[] arr)
+        {
+            for(int i=0 ;i<arr.Length-1 ; i++)
+            {
+                int min_pos = i;
+                for(int j = i + 1 ; j<arr.Length ; j++)
+                {
+                    if(arr[j] < arr[min_pos])
+                    {
+                        min_pos = j;
+                    }
+                }
+                swap(ref arr[i],ref arr[min_pos]);
+            }
+        }
+        static void swap(ref int a,ref int b)
+        {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+    }
+}
