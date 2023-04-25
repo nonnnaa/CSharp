9ff15b42785105c376fad004e9fd2a8c9c5c4bae
DESKTOP-4T2RKR7\admin <phungthinh18052003@gmail.com>
2023-04-25 20:51:26 +0700

    update

---
+++ Static_lesson/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Static_lesson
+{
+    class Student
+    {
+        private int number;
+        private string name;
+        private static string college = "PTIT";
+
+        public Student(int x, string s)
+        {
+            number = x;
+            name = s;
+        }
+        public void Display()
+        {
+            Console.WriteLine(number + " " + name + " " + college);
+        }
+        public static void Change()
+        {
+            college = "CodeGym";
+        }
+    }
+    class TestStaticMethod
+    {
+        public static void Test()
+        {
+             Student.Change();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] s)
+        {
+            TestStaticMethod.Test();
+            Student s1 = new Student(111, "Hoang");
+            s1.Display();
+        }
+    }
+}
