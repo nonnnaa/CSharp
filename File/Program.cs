cb0969528cc4c0708764977ed98b53d4dcd1c7f8
DESKTOP-4T2RKR7\admin <phungthinh18052003@gmail.com>
2023-05-04 00:00:31 +0700

    update

---
+++ File/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace File
+{
+    class ReadTextFileExample
+    {
+        void ReadTextFile(string filePath)
+        {
+            try
+            {
+                FileInfo file = new FileInfo(filePath);
+                if (!file.Exists)
+                {
+                    throw new FileNotFoundException();
+                }
+
+                StreamReader reader = new StreamReader(filePath);
+                string line = "";
+                int sum = 0;
+                line = reader.ReadLine();
+                while(line != null) 
+                {
+                    Console.WriteLine(line);
+                    sum += Int32.Parse(line);
+                    line = reader.ReadLine();
+                }
+                reader.Close();
+                Console.WriteLine("Total: " + sum);
+            }
+            catch (System.Exception)
+            {
+                Console.Error.WriteLine("File not found or invalid content");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter input file path : ");
+            string path = Console.ReadLine();
+            Console.WriteLine("File path : " + path);
+            ReadTextFileExample example = new ReadTextFileExample();
+            example.ReadTextFile(path);
+        }
+    }
+}
