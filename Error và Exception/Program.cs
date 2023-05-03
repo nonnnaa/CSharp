e9e283cae48ea2aadb66aaef90d4b9e4c45e8a69
DESKTOP-4T2RKR7\admin <phungthinh18052003@gmail.com>
2023-05-03 16:29:01 +0700

    update

---
+++ Error và Exception/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ErrorAndException
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random random  = new Random();
+            int[] numbers = new int[5];
+            for(int i=0 ;i<numbers.Length ; i++)
+            {
+                numbers[i] = random.Next(1, 101);
+            }
+            Console.WriteLine(numbers[numbers.Length]);
+            try{
+                Console.WriteLine(numbers[numbers.Length]);
+            }
+            catch(IndexOutOfRangeException ex){
+                Console.WriteLine(ex);
+            }
+        }
+
+    }
+}
